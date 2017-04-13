3aa95126d0fdcb3579adaf699e9878e2fe776a50
Vlad Ivanov <vlad@ivanov.email>
2017-04-13 20:26:16 +0300

    Update CM/Lineage naming
    Change-Id: I091bf23b644325fd9781fdb4a7ff7a7cf6968e88

--- lineage.mk
+++ lineage.mk
@@ -1,7 +1,7 @@
 # Release name
 PRODUCT_RELEASE_NAME := k11ntc_a
 
-# Inherit some common CM stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
@@ -9,7 +9,11 @@ $(call inherit-product, device/alps/k11ntc_a/device_k11ntc_a.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := k11ntc_a
-PRODUCT_NAME := cm_k11ntc_a
-PRODUCT_BRAND := alps
+PRODUCT_NAME := lineage_k11ntc_a
+PRODUCT_BRAND := Vernee
 PRODUCT_MODEL := k11ntc_a
 PRODUCT_MANUFACTURER := alps
+
+TARGET_VENDOR := alps
+TARGET_VENDOR_PRODUCT_NAME := Mars
+TARGET_VENDOR_DEVICE_NAME := k11ntc_a
